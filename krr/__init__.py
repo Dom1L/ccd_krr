b292117193a1a00fb6d08a8bd992f9ac1e7431e3
Dom1L <lemm.d@web.de>
2020-05-22 18:34:32 +0200

    rosenbrock opt results

--- krr/__init__.py
+++ krr/__init__.py
@@ -1,4 +1,4 @@
 from .representation import Compound
 from .train import learning_curve, split_data, rkernel_training
 from .utils import qm7_path
-import nelder_mead
+from .nelder_mead import minimize
